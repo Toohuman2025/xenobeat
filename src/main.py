e8deb95c7f20249334383c667dec270e512e514e
Toohuman2025 <micieloazulbonito@gmail.com>
2025-10-07 06:15:09 -0400

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-# XenoBeat - Reproductor de música pop mágico
-# Autor: Toohuman & Xenita Baby 💜🐾
-
-def main():
-    print("¡Bienvenido a XenoBeat! 🎵✨")
-    print("Reproductor pop limpio y mágico activado.")
-
-if __name__ == "__main__":
-    main()
